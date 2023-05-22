11dcf4fafdc3abd11bb5aee042b9220643581fa1
Emmanuel Monteiro <105503767+EmmanuelElion@users.noreply.github.com>
2023-05-22 15:02:10 -0300

    Update Projeto Aluguel carros.py
    Foi feito um update adicionando o loop a parte de Cadastro de carros! 
    
    É preciso revisar o loop na parte que vê a senha de administrador

--- Projeto Aluguel carros.py
+++ Projeto Aluguel carros.py
@@ -1,70 +1,131 @@
-import os
+# Criando as lisas necessárias para guardar as informações: 
+#Parte referente ao cadástro de veículos
+classe = []
+modelo = []
+marca  = []
+placa  = []
+cor    = []
+tipo   = []
+combustivel = []
+vlrDiaria   = []
+observacao  = []
 
-carros = [
-        ("Chevrolet Tracker", 120), 
-        ("Chevrolet Onix", 90), 
-        ("Chevrolet Spin", 150),
-        ("Hyundai HB20", 85), 
-        ("Hyundai Tucson", 120), 
-        ("Fiat Uno", 60), 
-        ("Fiat Mobi", 70), 
-        ("Fiat Pulse", 130)
-        ] # Lista dos carros presentes para alugar 
-alugados = [] 
+# Parte referente ao cadástro de clientes 
+nomes   = []
+cpf     = []
+tipoCnh = []
+numCNH  = []
+valiCNH = []
+ende    = []
+tel     = []
+atvProf = []
 
 
+# precis adicionar um loop de repetição ao cadastrar o carro p/ depois que cadastrar o programa perguntar o que fazer dnv
 
-def mostrar_lista_de_carros(lista_de_carros): # usando p/ definir o "metodo" mostrar lista de carros 
-    for i, car in enumerate(lista_de_carros): # metodo "enumerate" é responsavel por enumerar os carros presentes na lista 
-        print("[{}] {} - R$ {} /dia.".format(i, car[0], car[1])) # print responsavel por exibir os carros 
+tipoServico = int(input("1- Cadastrar carros | 2- Cadastrar clientes \n3- aluguel automoveis | 4- Devolução automoveis \nR: "))
+print("=" * 80)
+
+# Caso deseje cadastrar um carro 
+if tipoServico == 1:
+       
+    # começando com o cadastro de carros 
+    senhaAdm = 1234
+
+   
+    controleCadastroCarro = 1
+    while controleCadastroCarro > 0: 
+
+         
+         # 1.1: 
+        senha = int(input("Informe a sua senha: "))
+        # validando a senha 
+        if senha == senhaAdm: 
+            # se a senha estiver certa...   
+            # abrindo cadastro para os veículos 
+            classeVeiculo = int(input("Qual a classe do veículo? \n1- simples | 2- mediano | 3- luxuosos | 4- onibus e caminhões\nR: "))
+            print("=" * 80)
+            modeloVei = input("Informe qual o modelo do automovel: ")
+            print("=" * 80)
+            marcaVei = input("Qual a marca do veículo: ")
+            print("=" * 80)
+            placaVei = input("Insira a placa do automovel: ")
+            print("=" * 80)
+            corVei  = input("Insira a cor do automovel: ")
+            print("=" * 80)
+            tipoVei = int(input("1- SUV ----- 2- Ret    ----- 3- Caminhonete \n4- Van ----- 5- Onibus ----- 6- Caminhão \nR: "))
+            print("=" * 80)
+            combustivelVei = int(input("1- alcool \n2- gasolina \n3- hibrido \n4- eletrico \nR: "))
+            print("=" * 80)
+            vlrDiariaVei = float(input("Diária aluguel R$ "))
+            print("=" * 80)
+            observacaoVei = input("Observações: ")
+            print("=" * 80)
+
+
+            # Adicionando as informações nas listas necessárias 
+            classe.append(classeVeiculo)
+            modelo.append(modeloVei.upper())
+            marca.append(marcaVei.upper())
+            placa.append(placaVei.upper())
+            cor.append(corVei.upper())
+            tipo.append(tipoVei)
+            combustivel.append(combustivelVei)
+            vlrDiaria.append(vlrDiariaVei)
+            observacao.append(observacaoVei.upper())
+
+            print(f"O veiculo {modelo[-1]} foi gravado com sucesso!!")
+
+            continuarCadastroC = int(input("Deseja continuar cadastrando novos carros? (1- Sim | 2- Não) "))
+
+            if continuarCadastroC == 1: 
+                controleCadastroCarro += 1 
+            else:
+                print("Programa encerrado")
+                controleCadastroCarro = 0 
+
         
-
-while True: # "Equanto" for verdade que...
-    os.system("clear")
-    print("=========")
-    print("Bem vindo à locadora de carros!")
-    print("=========")
-    print("O que deseja fazer?")
-    print("0 - Mostrar portifólio | 1 - Alugar um carro | 2 - Devolver um carro")
-    op = int(input(": ")) # Perguntando qual a ação que o usuário quer fazer 
-
-    os.system("clear") 
-    if op == 0: # "Se" op for = 0 ... 
-        mostrar_lista_de_carros(carros) # retorna o "mostrar lista" carro 
-
-    elif op == 1: # Se o cliente quiser alugar um carro...
-        mostrar_lista_de_carros(carros)
-        print("==========")
-        print("Escolha o código do carro:")
-        cod_car = int(input("R:")) # recebendo a resposta de qual carro o cliente quer alugar 
-        print("Por quantos dias você deseja alugar este carro?")
-        dias = int(input("R:")) # recebendo a resposta de por quantos dias o usuário quer alugar 
-        os.system("clear")
-
-        print("Você escolheu {} por {} dias.".format(carros[cod_car][0], dias))
-        print("O aluguel totalizaria R$ {}. Deseja alugar?".format(dias * carros[cod_car][1]))
-
-        print("0 - SIM | 1 - NÃO")
-        conf = int(input())
-        if conf == 0:
-            print("Parabéns você alugou o {} por {} dias.".format(carros[cod_car][0], dias))
-            alugados.append(carros.pop(cod_car))
-
-    elif op == 2:
-        if len(alugados) == 0:
-            print("Não há carros para devolver.")
+        # depois observar o loop de erro de senha novamente!!!! 
         else:
-            print("Segue a lista de carros alugados. Qual você deseja devolver?")
-            mostrar_lista_de_carros(alugados)
-            print("")
-            print("Escolha o código do carro que deseja devolver:")
-            cod = int(input())
-            if conf == 0:
-                print("Obrigado por devolver o carro {}".format(alugados[cod][0]))
-                carros.append(alugados.pop(cod))
-    
-    print("")
-    print("===========")
-    print("0 para CONTINUAR | 1 para SAIR")
-    if float(input()) == 1:
-        break
+            print("Senha incorreta!")
+            erroSenha = 0      
+                
+            # O loop de errar a senha n está funcionando.... 
+            if erroSenha < 5: 
+                print("Tente a senha novamente")
+                controleCadastroCarro += 1
+                erroSenha += 1 
+            else: 
+                print(f"Você ja tentou entrar {erroSenha} vezes sem Sucesso \nPrograma encerrado")
+                controleCadastroCarro = 0
+                break
+
+
+
+# caso deseje cadastrar um cliente novo...     
+elif tipoServico == 2: 
+
+    nomeCli = input("Informe o nome: ")
+    cpfCli = int(input("Informe seu cpf: "))
+    tipoCNHcli = input("Qual o tipo da sua CNH: ")
+    numCNHcli = int(input("Informe o numero da sua CNH: "))
+    valiCNHcli = input("Informe a validade da sua CNH: ")
+    endereco = input("Informe seu endereço: ")
+    telefoneCli = int(input("Informe seu telefone: "))
+    atividadeCli = input("Informe sua profissão: ")
+
+
+    # Adicionando as informações necessárias 
+    nomes.append(nomeCli.upper())
+    cpf.append(cpfCli)
+    tipoCnh.append(tipoCNHcli.upper())
+    numCNH.append(numCNHcli)
+    valiCNH.append(valiCNHcli)
+    ende.append(endereco.upper())
+    tel.append(telefoneCli)
+    atvProf.append(atividadeCli.upper())
+
+    print(f"O cliente {nomes[-1]} foi gravado com sucesso!")
+
+
+     
